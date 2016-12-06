f88200415788c0c52acabc00d05d4067e27cdb3a
BoOkh <bohdan.okhrimchuk@gmail.com>
2016-12-06 22:59:52 +0200

    Release

--- app/Views/layouts/default.php
+++ app/Views/layouts/default.php
@@ -32,16 +32,16 @@
                     </div>
                     <div id="navbar" class="navbar-collapse collapse">
                         <ul class="nav navbar-nav">
-                            <li><a href="/">Home</a></li>
+                            <li class="<?php if(\App\Core\Request::uri() == '') echo 'active';?>"><a href="/">Home</a></li>
                             <?php if(isset($_SESSION['admin'])){ ?>
-                                <li><a href="/create">Create post</a></li>
+                                <li class="<?php if(\App\Core\Request::uri() == 'create') echo 'active';?>"><a href="/create">Create post</a></li>
                             <?php }?>
                         </ul>
                         <ul class="nav navbar-nav navbar-right">
                             <?php if(isset($_SESSION['admin'])){ ?>
                                 <li><a href="/logout"><span class="glyphicon glyphicon-log-out"> Logout</span></a></li>
                             <?php } else { ?>
-                                <li><a href="/login"><span class="glyphicon glyphicon-log-in"> Login</span></a></li>
+                                <li class="<?php if(\App\Core\Request::uri() == 'login') echo 'active';?>"><a href="/login"><span class="glyphicon glyphicon-log-in"> Login</span></a></li>
                             <?php }?>
                         </ul>
                     </div>
